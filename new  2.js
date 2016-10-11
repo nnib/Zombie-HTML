50532fb7d3acbc17df732073642e9db8f94e6853
Niamh Ní Bhriain <x14100274@student.ncirl.ie>
2016-10-11 12:18:23 +0100

    first commit

---
+++ new  2.js
@@ -0,0 +1,22 @@
+body, ul, li, img {
+	margin: 0; padding: 0; border: 0;
+}
+
+body {
+	background: #545454 url(images/bg.png);
+}
+
+#images{
+	width: 500px; margin: 200px auto;
+}
+
+#images li {
+	list-style: none; float: left; margin: 15px;
+}
+
+	#images a img {
+		border: 5px solid #aaa;
+	}
+		#images a:hover img {
+			border: 5px solid #75879d;
+		}
